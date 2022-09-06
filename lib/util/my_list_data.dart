efc28be2b8982f39f0e92633605e8c69d4b1269c
abdulrohman70 <ar886069@gmail.com>
2022-09-06 12:07:39 +0700

    modifikasi ui belum fix

--- lib/util/my_list_data.dart
+++ lib/util/my_list_data.dart
@@ -16,45 +16,49 @@ class MyListData extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Container(
-          height: 80.0,
-          padding: EdgeInsets.all(12),
-          decoration: BoxDecoration(
-            color: Colors.grey[100],
-            borderRadius: BorderRadius.circular(12),
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 25),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            height: 80.0,
+            padding: EdgeInsets.all(12),
+            decoration: BoxDecoration(
+              color: Colors.grey[100],
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Image.asset(iconImagePath),
           ),
-          child: Image.asset(iconImagePath),
-        ),
-        SizedBox(
-          width: 20,
-        ),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              listTitle,
-              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-            ),
-            SizedBox(
-              height: 12,
-            ),
-            Text(
-              listSubTitle,
-              style: const TextStyle(
-                fontSize: 14,
-                color: Colors.grey,
+          SizedBox(
+            width: 20,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                listTitle,
+                style:
+                    const TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
               ),
-            ),
-          ],
-        ),
-        SizedBox(
-          width: 35,
-        ),
-        Icon(Icons.arrow_forward_ios),
-      ],
+              SizedBox(
+                height: 12,
+              ),
+              Text(
+                listSubTitle,
+                style: const TextStyle(
+                  fontSize: 14,
+                  color: Colors.grey,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            width: 35,
+          ),
+          Icon(Icons.arrow_forward_ios),
+        ],
+      ),
     );
   }
 }
